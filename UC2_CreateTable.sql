5b9d76b278c8d22694238f622b9887e3f1563706
CMD <gautamkailashsingh@gmail.com>
2020-11-04 00:55:04 +0530

    [Gautam] Created an address book table

---
+++ UC2_CreateTable.sql
@@ -0,0 +1,15 @@
+--Use Case 1
+create database address_book_service
+--Use Case 2
+create table address_book
+(
+first varchar(25),
+last varchar(25),
+address varchar(25),
+city varchar(25),
+state varchar(25),
+zip int,
+phone varchar(25),
+email varchar(25)
+);
+select * from address_book;
